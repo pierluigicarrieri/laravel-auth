7cb3c615d98894f98bc9ab347b7701f9dca99851
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-10-09 00:07:57 +0200

    Create view done, added some basic css

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,39 +2,47 @@
 
 @section('content')
 
-<div class="container">
+<main>
 
-    <h1 class="py-5 text-center text-danger">Projects index page (admin)</h1>
+    <div class="container">
 
-    <div class="row row-cols-4 g-4">
+        <h1 class="py-3 text-center text-danger">Projects index page (admin)</h1>
 
-        @foreach ($projects as $project)
-            
-            <div class="col">
+        <div class="d-flex justify-content-center pb-3">
+            <a class="btn btn-primary" href="{{route('admin.projects.create')}}">Create New Project</a>
+        </div>
 
-                <div class="card h-100">
-                    <a href="{{route('admin.projects.show', $project['slug'])}}">
-                        <img src="{{$project['image']}}" class="card-img-top" alt="">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">{{$project['name']}}</h5>
-                        <p class="card-text">{{substr_replace($project['description'], '...', 100)}}</p>
+        <div class="row row-cols-4 g-4">
+
+            @foreach ($projects as $project)
+                
+                <div class="col">
+
+                    <div class="card h-100">
+                        <a href="{{route('admin.projects.show', $project['slug'])}}">
+                            <img src="{{$project['image']}}" class="card-img-top" alt="">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">{{$project['name']}}</h5>
+                            <p class="card-text">{{substr_replace($project['description'], '...', 100)}}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{$project['publication_date']}}</li>
+                            <li class="list-group-item">{{$project['technologies_used']}}</li>
+                            <li class="list-group-item">
+                                <a href="{{$project['git_link']}}">{{$project['git_link']}}</a>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">{{$project['publication_date']}}</li>
-                        <li class="list-group-item">{{$project['technologies_used']}}</li>
-                        <li class="list-group-item">
-                            <a href="{{$project['git_link']}}">{{$project['git_link']}}</a>
-                        </li>
-                    </ul>
+
                 </div>
 
-            </div>
+            @endforeach
 
-        @endforeach
+        </div>
 
     </div>
 
-</div>
+</main>
 
 @endsection
